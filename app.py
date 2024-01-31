9cf22b4728c3e8aca055746595971570f8dfb547
Hebert Ribeiro <50155910+Hebert031@users.noreply.github.com>
2024-01-30 22:23:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, request, jsonify, send_file
 import pandas as pd
 import os
 
+# Lista de cidades com taxa de 7%
+cidades_taxa_7 = [
+    "BELO HORIZONTE", "BETIM", "BRUMADINHO", "CONFINS", "CONTAGEM",
+    "ESMERALDAS", "IBIRITE", "IGARAPE", "JABOTICATUBAS", "LAGOA SANTA",
+    "MARIO CAMPOS", "MATEUS LEME", "NOVA LIMA", "RAPOSOS", "RIBEIRAO DAS NEVES",
+    "SABARA", "SANTA LUZIA", "SÃO JOAQUIM DE BICAS", "SARZEDO", "VESPASIANO", "SETE LAGOAS"
+]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,7 +32,7 @@ def calcular_tarifa_de_frete():
 
         try:
             # Carrega a planilha XLSX
-            df = pd.read_excel(file_path, sheet_name='TRADING CARE')
+            df = pd.read_excel(file_path)
             print(df.head())  # Imprime as primeiras linhas do DataFrame
             print(df.columns)  # Imprime os nomes das colunas do DataFrame
 
@@ -35,12 +43,12 @@ def calcular_tarifa_de_frete():
             if 'NF_VALOR' not in df.columns or 'MUNICIPIO' not in df.columns:
                 raise ValueError("Colunas 'NF_VALOR' e 'MUNICIPIO' são necessárias na planilha")
 
-            # Calcula a tarifa de frete com verificação do valor mínimo e arredondamento
-            df['Tarifa_de_Frete'] = df.apply(lambda row: calculate_freight(row['NF_VALOR'], row['MUNICIPIO']), axis=1)
+            # Calcula a tarifa de frete e arredonda para duas casas decimais
+            df['Tarifa_de_Frete'] = df.apply(lambda row: round(row['NF_VALOR'] * (0.07 if row['MUNICIPIO'].upper() in cidades_taxa_7 else 0.085) / 0.94, 2), axis=1)
 
             # Salva a nova planilha com a coluna de cálculo de frete
             output_file_path = 'planilha_com_frete.xlsx'
-            df.to_excel(output_file_path, index=False)
+            df.to_excel(output_file_path, index=False, float_format="%.2f")  # Define o formato float para duas casas decimais
 
             # Remove o arquivo temporário
             os.remove(file_path)
@@ -52,20 +60,6 @@ def calcular_tarifa_de_frete():
 
     else:
         return jsonify({'error': 'Formato de arquivo inválido, selecione uma planilha XLSX'}), 400
-
-
-def calculate_freight(nf_valor, municipio):
-    # Define os valores mínimos para o cálculo da taxa
-    valor_minimo_bh = 140
-    valor_minimo_outros = 240
-
-    # Verifica se o valor da nota fiscal é maior ou igual ao valor mínimo
-    if (municipio == 'Belo Horizonte' and nf_valor >= valor_minimo_bh) or (municipio != 'Belo Horizonte' and nf_valor >= valor_minimo_outros):
-        # Calcula a tarifa de frete com base no valor da nota fiscal e na localização
-        taxa = nf_valor * (0.07 if municipio == 'Belo Horizonte' else 0.085) / 0.94
-        return round(taxa, 2)  # Arredonda para duas casas decimais
-    else:
-        return 'Valor mínimo não atingido'
 
 
 if __name__ == '__main__':
